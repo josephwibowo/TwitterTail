3739040f330958f271b83e7e4e857b6e68a6442d
Joseph Wibowo <josephwibowo@gmail.com>
2020-06-01 20:37:06 -0700

    Add daemon file

--- kafka/vbox/vbox-install.sh
+++ kafka/vbox/vbox-install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# This Script installs Docker and SSH onto Ubuntu. Also sets up networking
 
 # Install OpenSSH and make it passwordless
 sudo apt-get update
@@ -7,7 +8,8 @@ sudo sed -i -e 's/#PermitEmptyPasswords no/PermitEmptyPasswords yes/g' /etc/ssh/
 sudo service sshd restart
 
 # rewrite network file for static IP
-sudo cp kafka/vbox/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
+user=$(whoami)
+sudo cp /home/${user}/Documents/TwitterTail/kafka/vbox/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
 sudo netplan apply
 
 # Docker Install (https://docs.docker.com/engine/install/ubuntu/)
@@ -26,6 +28,9 @@ sudo add-apt-repository \
    stable"
 
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo cp /home/${user}/Documents/TwitterTail/kafka/vbox/daemon.json /etc/docker/daemon.json
+sudo systemctl stop docker
+sudo systemctl start docker
 
 # add lubuntu to docker group
 sudo usermod -aG docker $(whoami)
