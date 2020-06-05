623009dbdbc31feed16e63729a862cefd8f63e1c
Joseph Wibowo <josephwibowo@gmail.com>
2020-06-04 20:37:26 -0700

    revert

--- kafka/vbox/worker-install.sh
+++ kafka/vbox/worker-install.sh
@@ -2,7 +2,7 @@
 
 IP1=${1:-192.168.0.50}
 IP2=${2:-192.168.0.51}
-if [ ${IP2} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]; then
+if [[ ${IP2} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
     sudo sed -i -e 's/${IP1}/${IP2}/g' /etc/netplan/01-network-manager-all.yaml
     sudo netplan apply
 else
