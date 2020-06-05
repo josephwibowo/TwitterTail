cf0e8cbbac2dd5cba61f4b7c01a7dc17686c29da
Joseph Wibowo <josephwibowo@gmail.com>
2020-06-04 20:56:16 -0700

    add sudo

--- kafka/vbox/worker-install.sh
+++ kafka/vbox/worker-install.sh
@@ -3,7 +3,7 @@
 IP1=${1:-192.168.0.50}
 IP2=${2:-192.168.0.51}
 if [[ ${IP2} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    sed -i '' "s/${IP1}/${IP2}/g" /etc/netplan/01-network-manager-all.yaml
+    sudo sed -i '' "s/${IP1}/${IP2}/g" /etc/netplan/01-network-manager-all.yaml
     sudo netplan apply
 else
     echo "Invalid IP"
